5f7f317e1621f85cdae23e16f301ae3558d7285c
dvberkel <daan.v.berkel.1980@gmail.com>
2011-03-30 08:03:55 +0200

    Added characters to the mix.
    Characters are now recognized in the Smalltalk brush.

--- shBrushSmalltalk.js
+++ shBrushSmalltalk.js
@@ -6,7 +6,7 @@ SyntaxHighlighter.brushes.Smalltalk = function()
 		{ regex: SyntaxHighlighter.regexLib.multiLineDoubleQuotedString, css: 'comments' }, // comments
 		{ regex: SyntaxHighlighter.regexLib.multiLineSingleQuotedString, css: 'string' },   // string
 		{ regex: /\b\d+(\.\d+)?\b/gi,                                    css: 'value' },    // numbers
-		{ regex: /#\w+/gi,                                               css: 'constants'}, // symbols
+		{ regex: /[$#]\w+/gi,                                            css: 'constants'}, // characters, symbols
 		{ regex: /:\w+/g,                                                css: 'variable' }, // block paramaters
 		{ regex: /\w+:/g,                                                css: 'functions'}, // keyword messages
 		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),           css: 'keyword' }   // reserved words
